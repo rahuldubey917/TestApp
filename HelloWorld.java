8fd3437524367b8f4098eea12d23aaf747320435
rahuldubey917 <39078893+rahuldubey917@users.noreply.github.com>
2018-06-09 22:19:02 +0530

    Swap of two variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,24 @@
 package ForGit;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter a num");
+		int a=sc.nextInt();
+		
+		System.out.println("enter second num");
+		int b=sc.nextInt();
+		
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.print("a: " + a + "\nb:" + b);
 		System.out.print("Hello world, this is a java program");
+		sc.close();
 	}
 
 }
