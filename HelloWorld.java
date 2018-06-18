34b1ae7ff7854754d97c1d797e2a00e05bc067d0
Rahul Dubey <39078893+rahuldubey917@users.noreply.github.com>
2018-06-18 10:56:39 +0530

    Example Program
    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,8 @@
-package ForGit;
+public class HelloWorld{
+public static void main(String args[])
+{
 
-import java.util.Scanner;
+System.out.println("hello world");
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		
-		Scanner sc= new Scanner(System.in);
-		System.out.println("enter a num");
-		int a=sc.nextInt();
-		
-		System.out.println("enter second num");
-		int b=sc.nextInt();
-		
-		a = a + b;
-		b = a - b;
-		a = a - b;
-		System.out.print("a: " + a + "\nb:" + b);
-		System.out.print("Hello world, this is a java program");
-		sc.close();
-	}
-
+}
 }
